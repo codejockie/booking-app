802a74f659341a95ff62648725c179abdc04bb31
John C. Kennedy <devjckennedy@gmail.com>
2019-05-29 21:35:30 +0900

    initial client app setup

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { BookingComponent } from './booking/booking.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'booking', component: BookingComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
