7c471bb0f3c4019bda0cc6f7ebc897d17fca4f5b
John C. Kennedy <devjckennedy@gmail.com>
2019-06-01 08:51:33 +0900

    add dummy data

---
+++ server/src/data/bookings.ts
@@ -0,0 +1,32 @@
+export const bookings = [
+  {
+    city: "Munich",
+    id: "weeoke",
+    propertyId: "wer45",
+    propertyName: "Munich Marriott Hotel",
+    user: {
+      id: "xyz",
+      name: "John",
+    },
+  },
+  {
+    city: "Munich",
+    id: "weeoke",
+    propertyId: "rty43",
+    propertyName: "Hilton Munich Park",
+    user: {
+      id: "xyz",
+      name: "John",
+    },
+  },
+  {
+    city: "Munich",
+    id: "weeoke",
+    propertyId: "rty43",
+    propertyName: "Hilton Munich Park",
+    user: {
+      id: "abc",
+      name: "John",
+    },
+  },
+];
---
+++ server/src/data/properties.ts
@@ -0,0 +1,12 @@
+export const properties = [
+  {
+    city: "Munich",
+    id: "wer45",
+    name: "Munich Marriott Hotel",
+  },
+  {
+    city: "Munich",
+    id: "rty43",
+    name: "Hilton Munich Park",
+  },
+];
