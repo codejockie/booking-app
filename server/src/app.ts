e4c9db55a3c04115acf586a86a57e11ae6234979
John C. Kennedy <devjckennedy@gmail.com>
2019-06-01 17:35:38 +0900

    implement login

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,34 @@
 import { json, urlencoded } from "body-parser";
+import cors = require("cors");
 import express = require("express");
+import { sign } from "jsonwebtoken";
+import mongoose = require("mongoose");
+import winston = require("winston");
 
+import { User } from "./models/User";
 import { routes } from "./routes";
+import { seedUsers } from "./seed";
+
+mongoose.Promise = global.Promise;
 
 const app = express();
 const router = express.Router();
 const PORT = process.env.PORT || 3000;
+const { JWT_SECRET = "todo-app-super-shared-secret" } = process.env;
+const logger = winston.createLogger({
+  defaultMeta: { service: "user-service" },
+  format: winston.format.json(),
+  level: "info",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+  ],
+});
 
 // Configure middleware
 app.use(urlencoded({ extended: true }));
 app.use(json());
+app.use(cors());
 
 // Enable CORS
 app.use((request, response, next) => {
@@ -22,10 +41,22 @@ app.use((request, response, next) => {
 routes(router);
 app.use("/", router);
 
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
+app.post("/auth/login", async (req, res) => {
+  const { password, email } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user.comparePassword(password)) {
+    return res.sendStatus(401);
+  }
+  const token = sign({ userID: user._id }, JWT_SECRET, { expiresIn: "2h" });
+  res.send({ token });
 });
 
-app.listen(PORT);
+mongoose.connect("mongodb://localhost:27017/lime", { useCreateIndex: true, useNewUrlParser: true }, () => {
+  // Seed users on Connect
+  seedUsers();
+});
+
+app.listen(PORT, () => logger.info(`Server running on localhost:${PORT}`));
 
 export default app;
