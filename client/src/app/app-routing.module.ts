3d9292485189ae34422bfb3505e470c78b1348dd
John C. Kennedy <devjckennedy@gmail.com>
2019-06-02 10:14:46 +0900

    update routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './auth.guard';
+
 import { BookingComponent } from './booking/booking.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: 'booking', component: BookingComponent },
-  { path: '**', component: HomeComponent }
+  { path: 'bookings/:propertyId', component: BookingComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
