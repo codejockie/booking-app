4fd5504267d563d76e228e4da85f51aad562f380
John C. Kennedy <devjckennedy@gmail.com>
2019-06-02 10:15:33 +0900

    implement booking

--- client/src/app/booking/booking.component.ts
+++ client/src/app/booking/booking.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { BookingService } from '../booking.service';
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['propertyId', 'propertyName', 'city', 'user'];
+  dataSource: any[];
+  constructor(private router: Router, private route: ActivatedRoute, private bookingService: BookingService) { }
 
   ngOnInit() {
+    const { propertyId } = this.route.snapshot.params;
+    this.bookingService.getBookings(propertyId).subscribe((bookings: any[]) => {
+      this.dataSource = bookings;
+    });
   }
 
 }
