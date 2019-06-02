fab0c16359ca1de5d937a445ee2bb618779c5ae7
John C. Kennedy <devjckennedy@gmail.com>
2019-06-02 10:03:58 +0900

    implement create booking action

--- server/src/controllers/BookingController.ts
+++ server/src/controllers/BookingController.ts
@@ -1,10 +1,19 @@
-import { Request, Response } from "express";
-import { bookings } from "../data/bookings";
+import { Response } from "express";
+
+import { Booking } from "../models/Booking";
+import { IRequest } from "./IRequest";
 
 export class BookingController {
-  public static getBookings(request: Request, response: Response) {
+  public static async getBookings(request: IRequest, response: Response) {
     const { propertyId } = request.params;
-    const allBookings = bookings.filter((booking) => booking.propertyId === propertyId);
+    const allBookings = await Booking.find({ propertyId });
     response.send(allBookings);
+  }
+
+  public static async createBooking(request: IRequest, response: Response) {
+    const { userID } = request.user;
+    const booking = await new Booking({ ...request.body, user: userID }).save()
+    .catch((error) => response.status(500).send({ error: "Error creating booking" }));
+    response.status(201).send({ status: "ok", booking });
   }
 }
